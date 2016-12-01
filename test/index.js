712ac00ca1f0185d4588fca4909c849b8c84c658
Ben Leen <ben_leen@mckinsey.com>
2016-12-01 16:51:34 +0100

    Added additional tests and fixed a bug related to null values

--- test/index.js
+++ test/index.js
@@ -27,6 +27,26 @@ describe('Errors', () => {
                 return done();
             }
 
+            expect(result).to.equal({ name: 'Error', stack: 'Some\nstack', message: 'foo' });
+        });
+
+        const err = new Error('foo');
+        err.stack = 'Some\nstack';
+        stream.end(err);
+    });
+
+    it('converts errors on a property to object literals', (done) => {
+
+        const stream = new Errors({});
+
+        stream.on('readable', () => {
+
+            const result = stream.read();
+
+            if (!result) {
+                return done();
+            }
+
             expect(result).to.equal({ data: { name: 'Error', stack: 'Some\nstack', message: 'foo' } });
         });
 
@@ -110,5 +130,78 @@ describe('Errors', () => {
         });
 
         stream.end('test');
+    });
+
+    it('accepts null', (done) => {
+
+        const stream = new Errors({});
+
+        stream.on('readable', () => {
+
+            const result = stream.read();
+
+            if (!result) {
+                return done();
+            }
+
+            expect(result).to.equal(null);
+        });
+
+        stream.end(null);
+    });
+
+    it('accepts objects with null properties', (done) => {
+
+        const stream = new Errors({});
+
+        stream.on('readable', () => {
+
+            const result = stream.read();
+
+            if (!result) {
+                return done();
+            }
+
+            expect(result).to.equal({ x: null });
+        });
+
+        stream.end({ x: null });
+    });
+
+
+    it('accepts undefined', (done) => {
+
+        const stream = new Errors({});
+
+        stream.on('readable', () => {
+
+            const result = stream.read();
+
+            if (!result) {
+                return done();
+            }
+
+            expect(result).to.equal(undefined);
+        });
+
+        stream.end(undefined);
+    });
+
+    it('accepts objects with undefined properties', (done) => {
+
+        const stream = new Errors({});
+
+        stream.on('readable', () => {
+
+            const result = stream.read();
+
+            if (!result) {
+                return done();
+            }
+
+            expect(result).to.equal({ x: undefined });
+        });
+
+        stream.end({ x: undefined });
     });
 });
